ec4712cb9c0fa6520c3544a3f01e522f7797c05f
Priyanka Saba <112psaba112@gmail.com>
2019-04-24 11:57:12 -0700

    added very very basic examples

---
+++ project/examples.py
@@ -0,0 +1,34 @@
+
+
+def getMissionFiles():
+    #getting mission files, which are width by length by height, all are the same length
+    files = dict()
+    #one block on ground
+    files[1] = [[[None, None, None, None, "stone", None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None]]
+                ]
+    #several disconnected blocks on ground
+    files[2] = [[[None, "stone", None, None, "stone", None, None, "stone", None, None],
+                [None, None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None]]
+                ]
+    #two level tower
+    files[3] =[[[None, None, None, None, "stone", None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None]],
+                [[None, None, None, None, "stone", None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None]],
+                ]
+    #three level tower
+    files[3] =[[[None, None, None, None, "stone", None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None]],
+                [[None, None, None, None, "stone", None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None]],
+                [[None, None, None, None, "stone", None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None]],
+                ]
