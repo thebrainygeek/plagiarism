b50ac85e9eeb61e170cdfaea3eff08f5ecf9e43f
Priyanka Saba <112psaba112@gmail.com>
2019-05-06 14:01:03 -0700

    integermissions

--- project/mission/mission3.py
+++ project/mission/mission3.py
@@ -1,4 +1,4 @@
 missionFile = dict()
-missionFile[tuple(4.5, 0.5, 4.5)] = "stone"
-missionFile[tuple(4.5, 1.5, 4.5)] = "stone"
-missionFile[tuple(4.5, 2.5, 4.5)] = "stone"
+missionFile[tuple(5, 0, 5)] = "stone"
+missionFile[tuple(5, 1, 5)] = "stone"
+missionFile[tuple(5, 2, 5)] = "stone"
