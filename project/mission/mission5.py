4ddbce344b7516a2d8e47481a2d8a4ccf45a9fc3
thebrainygeek <adityg2@uci.edu>
2019-05-23 12:17:49 -0700

    House Mission and Block Type Support Added

---
+++ project/mission/mission5.py
@@ -0,0 +1,57 @@
+missionFile = dict()
+
+missionFile[(5, 4, 227)] = "brick_block"
+missionFile[(5, 5, 227)] = "brick_block"
+missionFile[(5, 6, 227)] = "brick_block"
+missionFile[(5, 7, 227)] = "emerald_ore"
+missionFile[(5, 8, 227)] = "brick_block"
+missionFile[(5, 9, 227)] = "brick_block"
+missionFile[(5, 10, 227)] = "brick_block"
+
+missionFile[(5, 4, 228)] = "brick_block"
+missionFile[(5, 5, 228)] = "brick_block"
+missionFile[(5, 6, 228)] = "brick_block"
+missionFile[(5, 7, 228)] = "emerald_ore"
+missionFile[(5, 8, 228)] = "brick_block"
+missionFile[(5, 9, 228)] = "brick_block"
+missionFile[(5, 10, 228)] = "brick_block"
+
+missionFile[(5, 4, 229)] = "brick_block"
+missionFile[(5, 5, 229)] = "brick_block"
+missionFile[(5, 6, 229)] = "brick_block"
+missionFile[(5, 7, 229)] = "brick_block"
+missionFile[(5, 8, 229)] = "brick_block"
+missionFile[(5, 9, 229)] = "brick_block"
+missionFile[(5, 10, 229)] = "brick_block"
+
+
+missionFile[(5, 4, 230)] = "stone"
+missionFile[(5, 5, 230)] = "glass"
+missionFile[(5, 6, 230)] = "stone"
+missionFile[(5, 7, 230)] = "stone"
+missionFile[(5, 8, 230)] = "stone"
+missionFile[(5, 9, 230)] = "glass"
+missionFile[(5, 10, 230)] = "stone"
+
+missionFile[(5, 4, 231)] = "stone"
+missionFile[(5, 5, 231)] = "stone"
+missionFile[(5, 6, 231)] = "stone"
+missionFile[(5, 7, 231)] = "stone"
+missionFile[(5, 8, 231)] = "stone"
+missionFile[(5, 9, 231)] = "stone"
+missionFile[(5, 10, 231)] = "stone"
+
+
+missionFile[(5, 5, 232)] = "stone"
+missionFile[(5, 6, 232)] = "stone"
+missionFile[(5, 7, 232)] = "stone"
+missionFile[(5, 8, 232)] = "stone"
+missionFile[(5, 9, 232)] = "stone"
+
+missionFile[(5, 6, 233)] = "stone"
+missionFile[(5, 7, 233)] = "stone"
+missionFile[(5, 8, 233)] = "stone"
+
+missionFile[(5, 7, 234)] = "stone"
+
+
